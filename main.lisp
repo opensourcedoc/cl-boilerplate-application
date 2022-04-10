0cceca2d8b6288102a4e549e1a28df549c685c70
Michelle Chen <cwchen.tw@outlook.com>
2022-04-11 07:40:08 +0800

    Refactor some code

--- main.lisp
+++ main.lisp
@@ -4,20 +4,21 @@
 ;; Load other third-party Common Lisp packages here.
 
 
+(import 'cl-portable::argument-script)
 #+(or sbcl ccl clisp) (import 'cl-portable::compile-program)
-#+(or sbcl ccl clisp) (import 'cl-portable::platform)
+(import 'cl-portable::platform)
 (import 'cl-portable::quit-with-status)
 
 ;; Import other third-party Common Lisp functions or macros here.
 
 
 ;; Set your executable name.
-#+(or sbcl ccl clisp) (defconstant +program+ "program")
+(defconstant +program+ "program")
 
 ;; Declare your local functions here.
 
 ;; Simulate a main function.
-(defun main ()
+(defun main (argv)
     ;; Implement your main program here.
     (write-line "Hello World")
     #+ccl (finish-output)  ; Trick for Clozure CL.
@@ -26,20 +27,23 @@
     (quit-with-status 0))
 
 ;; Set a proper executable name according to current OS.
-#+(or sbcl ccl clisp) 
-    (if (equal :windows (platform))
-        (defvar *program* (concatenate 'string +program+ ".exe"))
-        (defvar *program* +program+))
+(if (equal :windows (platform))
+    (defvar *program* (concatenate 'string +program+ ".exe"))
+    (defvar *program* +program+))
+
+;; Starter of the program.
+(defun init ()
+  (main (cons *program* (cdr (argument-script)))))
 
 ;; Compile the program.
 #+(or sbcl ccl clisp)
   (if (equal :windows (platform))
       ;; It is merely supported to compile GUI programs
       ;;  in SBCL on Windows.
-      (compile-program *program* #'main :type :console)
-      (compile-program *program* #'main))
+      (compile-program *program* #'init :type :console)
+      (compile-program *program* #'init))
 #+(or sbcl ccl clisp) (quit-with-status)
 
 ;; Run the main function on-the-fly
 ;;  if no compliation mode.
-#-(or sbcl ccl clisp) (main)
+#-(or sbcl ccl clisp) (init)
